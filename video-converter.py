8ade65b5ea5dc033f02c4fb6575096eaa5fefbb9
Andre Habermann <andre.ha52@gmail.com>
2017-01-29 14:20:27 +0100

    weitere anpassungen

--- video-converter.py
+++ video-converter.py
@@ -9,11 +9,23 @@ for root, subFolders, files in os.walk(rootdir):
     for file in files:
         theFile = os.path.join(root, file)
         fileName, fileExtension = os.path.splitext(theFile)
-        if fileExtension.lower() in ('.avi', '.divx', '.flv', '.m4v', '.mp4', '.mov', '.mpg', '.mpeg', '.wmv'):
+        if fileExtension.lower() in ('.avi', '.divx', '.flv', '.m4v', '.mov', '.mpg', '.mpeg', '.wmv'):
             print('Adding', theFile)
             fileList.append(theFile)
 
-runstr = 'HandBrakeCLI -i "{0}" -o "{1}" --preset="Normal" --two-pass --turbo'
+runstr = '/usr/bin/HandBrakeCLI -i {0} -o {1} --preset="Normal" --two-pass --turbo'
+runlist = []
+runlist.append("/usr/bin/HandBrakeCLI")
+runlist.append("-i")
+runlist.append("inFile")
+runlist.append("-o")
+runlist.append("outFile")
+runlist.append("--preset=\"Normal\"")
+runlist.append("-e")
+runlist.append("x264")
+runlist.append("--vfr")
+#runlist.append("--two-pass")
+#runlist.append("--turbo")
 
 print('=======--------=======')
 
@@ -21,9 +33,13 @@ while fileList:
     inFile = fileList.pop()
     fileName, fileExtension = os.path.splitext(inFile)
     outFile = fileName+'.mkv'
+    runlist[2] = inFile
+    runlist[4] = outFile
 
     print('Processing', inFile)
-    returncode = subprocess.call(runstr.format(inFile, outFile))
+    process = subprocess.run(runlist)
+    print(process.returncode)
     time.sleep(5)
+    break
     print('Removing', inFile)
-    os.remove(inFile)
+    #os.remove(inFile)
